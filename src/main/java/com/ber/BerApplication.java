4b9c2281f39aba4347d44d48e5d59eaf2cd1db60
esfuerzo <dkdldrha@naver.com>
2024-08-15 18:01:05 +0900

    first commit

---
+++ src/main/java/com/ber/BerApplication.java
@@ -0,0 +1,13 @@
+package com.ber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ber/BerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
